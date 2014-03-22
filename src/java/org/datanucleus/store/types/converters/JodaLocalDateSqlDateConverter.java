8f3c99da7fbc8baa180daa6971c6e1100424b224
andyjefferson <andy@datanucleus.org>
2014-03-22 16:36:36 +0000

    [NUCJODATIME-18] Further Jodatime TypeConverters

---
+++ src/java/org/datanucleus/store/types/converters/JodaLocalDateSqlDateConverter.java
@@ -0,0 +1,50 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+   ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.sql.Date;
+
+import org.joda.time.LocalDate;
+
+/**
+ * TypeConverter from Joda LocalDate to java.sql.Date.
+ */
+public class JodaLocalDateSqlDateConverter implements TypeConverter<LocalDate, Date>
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toDatastoreType(java.lang.Object)
+     */
+    @SuppressWarnings("deprecation")
+    public Date toDatastoreType(LocalDate ld)
+    {
+        return new Date(ld.getYear(), ld.getMonthOfYear(), ld.getDayOfMonth());
+    }
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toMemberType(java.lang.Object)
+     */
+    public LocalDate toMemberType(Date date)
+    {
+        if (date == null)
+        {
+            return null;
+        }
+
+        return new LocalDate(date.getTime());
+    }
+}
---
+++ src/java/org/datanucleus/store/types/converters/JodaLocalTimeSqlTimeConverter.java
@@ -0,0 +1,49 @@
+/**********************************************************************
+Copyright (c) 2014 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+   ...
+**********************************************************************/
+package org.datanucleus.store.types.converters;
+
+import java.sql.Time;
+
+import org.joda.time.LocalTime;
+
+/**
+ * TypeConverter from Joda LocalTime to java.sql.Time.
+ */
+public class JodaLocalTimeSqlTimeConverter implements TypeConverter<LocalTime, Time>
+{
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toDatastoreType(java.lang.Object)
+     */
+    @SuppressWarnings("deprecation")
+    public Time toDatastoreType(LocalTime lt)
+    {
+        return new Time(lt.getHourOfDay(), lt.getMinuteOfHour(), lt.getSecondOfMinute());
+    }
+
+    /* (non-Javadoc)
+     * @see org.datanucleus.store.types.converters.TypeConverter#toMemberType(java.lang.Object)
+     */
+    public LocalTime toMemberType(Time time)
+    {
+        if (time == null)
+        {
+            return null;
+        }
+        return new LocalTime(time.getTime());
+    }
+}
